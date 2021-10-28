41dd2894fd1eca8b2c60a40fe92eab862df40d9c
Antonio Bennett <bennettantonio199@gmail.com>
2021-10-28 02:37:37 -0400

    testing date format

--- api/schema/schema.js
+++ api/schema/schema.js
@@ -73,6 +73,15 @@ const PropertyType = new GraphQLObjectType({
         if (req) {
           let tenants = [];
           for (const tenant of parent.residentIds) {
+            const event = await Event.findById(tenant);
+            console.log(event.toBeCompleted);
+
+            // const date = event.toBeCompleted;
+            // const today = date.getDate();
+            // const dayOfTheWeek = date.getDay();
+            // const newDate = date.setDate(today - (dayOfTheWeek || 7));
+            // return new Date(newDate);
+
             tenants.push(await User.findById(tenant));
           }
           return tenants;
